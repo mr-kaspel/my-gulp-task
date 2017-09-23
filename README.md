7b6fff7e3e566f11e5fbf4f6efe7ae7fc911f4e8
Andrew k-mp <EndryNiksan@yandex.com>
2017-09-23 12:44:03 +0500

    new settings

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 my start with git / build on gulp / 
 
 Тестовый проект для общего понимания работы с github.
+
+##### Структура проекта
+
+app-|
+    |_css
+    |_gonts
+    |_img
+    |_js
+    |_libs
+    |_sass
+    |_index.html
+
+dist-|
+     |_...
+     ...
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'), //подключает пакеты из папки node_modules в наш проект
+		babel = require('gulp-babel'),
 		sass = require('gulp-sass'), //Подключаем Sass пакет
 		browserSync = require('browser-sync'), // Подключаем Browser Sync отслеживания состояния файлов обновление в браузере, локальный сервер
 		concat = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
@@ -17,21 +18,33 @@ var gulp = require('gulp'), //подключает пакеты из папки 
 //Инструкция
 gulp.task('help', function() { //команда для запуска проекта "gulp mytask" вместо указания файла
 		console.log('\x1b[33m%s\x1b[0m', '                Привет, я таск!\n ----------------------');
-		console.log('1. (\x1b[33msass\x1b[0m) приобразует .sass в .css');
-		console.log('2. (\x1b[33mcss\x1b[0m) сжатие файлов .css (удаляет веб префиксы)');
-		console.log('3. (\x1b[33mcsso\x1b[0m) минификация файлов .css (не удаляет веб префиксы)');
-		console.log('4. (\x1b[33mun-css\x1b[0m) удаление не используемух селекторов');
-		console.log('5. (\x1b[33mscripts\x1b[0m) сжимает/конкатенация .js файлы и libs.');
-		console.log('6. (\x1b[33mimg\x1b[0m) сжимает изображений с учётом кэширования.(GIF, JPEG, PNG, SVG)');
-		console.log('7. (\x1b[33msvg\x1b[0m) сжимает SVG.');
-		console.log('8. (\x1b[33mwatch\x1b[0m) активирует отслеживание за изменениями файлов проекта.');
-		console.log('9. (\x1b[33mbrowser-sync\x1b[0m) активирует локальный сервер.');
-		console.log('10. Для установки библиотек введите (\x1b[33mbower i jquery\x1b[0m) (\x1b[33mbower i magnific-popup\x1b[0m)');
-		console.log('11. (\x1b[33mclear\x1b[0m) чистим папку dist');
-		console.log('12. (\x1b[33mclear-g\x1b[0m) чистим кеш Gulp');
+		console.log('1. (\x1b[33mready\x1b[0m) сборка итогового проекта в dist');
+		console.log('2. (\x1b[33mbabel\x1b[0m) старый js');
+		console.log('3. (\x1b[33msass\x1b[0m) приобразует .sass в .css');
+		console.log('4. (\x1b[33mcss\x1b[0m) сжатие файлов .css (удаляет веб префиксы)');
+		console.log('5. (\x1b[33mcsso\x1b[0m) минификация файлов .css (не удаляет веб префиксы)');
+		console.log('6. (\x1b[33mun-css\x1b[0m) удаление не используемух селекторов');
+		console.log('7. (\x1b[33mscripts\x1b[0m) сжимает/конкатенация .js файлы и libs.');
+		console.log('8. (\x1b[33mimg\x1b[0m) сжимает изображений с учётом кэширования.(GIF, JPEG, PNG, SVG)');
+		console.log('9. (\x1b[33msvg\x1b[0m) сжимает SVG.');
+		console.log('10. (\x1b[33mwatch\x1b[0m) активирует отслеживание за изменениями файлов проекта.');
+		console.log('11. (\x1b[33mbrowser-sync\x1b[0m) активирует локальный сервер.');
+		console.log('12. Для установки библиотек введите (\x1b[33mbower i jquery\x1b[0m) (\x1b[33mbower i magnific-popup\x1b[0m)');
+		console.log('13. (\x1b[33mclear\x1b[0m) чистим папку dist');
+		console.log('14. (\x1b[33mclear-g\x1b[0m) чистим кеш Gulp');
+		console.log('Для проверки пакетов на обновление команда (\x1b[33mncu\x1b[0m)');
 		
 });
 
+gulp.task('babel', function(){
+	return gulp.src('app/js/**/*.js')
+		.pipe(babel({
+			presets: ['env']
+		}))
+		.pipe(gulp.dest('dist/js'))
+});
+
+//sass компилирование css
 gulp.task('sass', function(){ // Создаем таск "sass"
 		return gulp.src('app/sass/**/*.sass') // Берем источник
 				.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
@@ -94,7 +107,7 @@ gulp.task('un-css', function () {
 });
 
 //Следим за изменениями
-gulp.task('watch', ['browser-sync', 'sass', 'css-libs', 'scripts'], function() {
+gulp.task('watch', ['browser-sync', 'sass', 'scripts'], function() {
 		gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами
 		gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
 		gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
@@ -128,4 +141,20 @@ gulp.task('clear', function() {
 //Автаматическая очистка кеша Gulp
 gulp.task('clear-g', function() {
 		return cache.clearAll(); //очистка кеша
+});
+
+gulp.task('ready', function(){
+	var buildCss = gulp.src([
+		'app/css/*.css'
+		])
+	.pipe(gulp.dest('dist/css'))
+
+	var buildFonts = gulp.src('app/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts'))
+
+	var buildJs = gulp.src('app/js/**/*')
+	.pipe(gulp.dest('dist/js'))
+
+	var buildHtml = gulp.src('app/*.html')
+	.pipe(gulp.dest('dist'));
 });
